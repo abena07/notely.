44cf15f263dfbc87b118801e77900f3441700bdb
abena07 <abenabennett@gmail.com>
2022-10-14 17:13:39 +0000

    feat::route to get all notes

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+   title = models.CharField(max_length=60)
+   content = models.CharField(max_length=60)
+   def __str__(self):
+       return self.name
+
--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import NoteSerializer
+from .models import Note
 
 # Create your views here.
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all().order_by('title')
+    serializer_class = NoteSerializer
