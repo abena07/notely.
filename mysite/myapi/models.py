37d540bed887359381000cabbbd54d6d58f23c98
abena07 <abenabennett@gmail.com>
2022-10-18 11:40:36 +0000

    wip

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -5,5 +5,5 @@ class Note(models.Model):
    title = models.CharField(max_length=60)
    content = models.CharField(max_length=60)
    def __str__(self):
-       return self.name
+       return self.title
 
