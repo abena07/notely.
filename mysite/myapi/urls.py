2e7f18127c4ffbc09f75a8abc76a401a9d1d6ca1
abena07 <abenabennett@gmail.com>
2022-10-25 16:23:00 +0000

    progress

--- mysite/myapi/urls.py
+++ mysite/myapi/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', views.note_list, name='note_list'),
     path('note/<int:pk>/', views.note_detail, name='note_detail'),
     path('note/new/', views.note_new, name='note_new'),
-    path('note/<int:pk>/edit/', views.note_edit, name='note_edit')
+    path('note/<int:pk>/edit/', views.note_edit, name='note_edit'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,8 +58,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+       'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
